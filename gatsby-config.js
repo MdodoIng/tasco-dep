733e42439ca7b34ffbfab088f0f645a7e10a030a
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-03-11 12:00:01 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit. 	new file:   .nvmrc 	new file:   bun.lockb 	new file:   gatsby-browser.js 	modified:   gatsby-config.js 	deleted:    package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	new file:   src/assets/coming soon .webp 	renamed:    src/images/icon.png -> src/assets/icon.png 	new file:   src/assets/logo.svg 	renamed:    src/pages/404.js -> src/pages/404.jsx 	deleted:    src/pages/index.js 	new file:   src/pages/index.jsx 	new file:   src/styles/global.css 	new file:   tailwind.config.js 	new file:   yarn.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,26 +4,36 @@
 module.exports = {
   siteMetadata: {
     title: `tasco`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/assets/icon.png",
+      },
     },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "assets",
+        path: "./src/assets/",
+      },
+      __key: "assets",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
 };
---
+++ src/pages/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import heroImage from "../assets/coming soon .webp";
+import { MainImage } from "gatsby-plugin-image";
+
+const index = () => {
+  return (
+    <div className="flex items-center justify-center flex-col m-8 h-screen">
+      <MainImage
+        src={logo}
+        width={100}
+        height={100}
+        alt="logo"
+        className="object-contain object-center w-auto h-20"
+      />
+      <MainImage
+        src={heroImage}
+        width={100}
+        height={100}
+        alt="logo"
+        className="object-contain object-center w-auto h-[70%]"
+      />
+    </div>
+  );
+};
+
+export default index;
