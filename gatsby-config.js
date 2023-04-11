715db4bf30aa25e2cacfcf858987464cd7706894
ameenSpeakerbox <ameen466speakerbox@gmail.com>
2023-04-11 17:04:32 +0530

    modified:   gatsby-config.js new file:   src/assets/favicon.png modified:   gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,33 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/assets/logo.svg",
+        name: `The Cool Application`,
+        short_name: `Cool App`,
+        description: `The application does cool things and makes your life better.`,
+        lang: `en`,
+        display: `standalone`,
+        icon: "src/assets/favicon.png",
+
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#fff`,
+        icons: [
+          {
+            src: `src/assets/favicon.png`,
+            sizes: `192x192`,
+            type: "image/png",
+            purpose: `maskable`,
+          },
+          {
+            src: `src/assets/favicon.png`,
+            sizes: `512x512`,
+            type: "image/png",
+            purpose: `maskable`,
+          },
+        ],
+        icon_options: {
+          purpose: `any maskable`,
+        },
       },
     },
     {
